73cda8a64fe4551faafc2807265f6a915a6df07b
Vladimir Damov <vdamov@github.com>
2019-01-25 15:31:35 +0200

    opa

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         port: process.env.PORT || 3000,
-        dbPath: 'mongodb://localhost:27017/rubiks-db'
+        dbPath: 'mongodb://rubiks-db:zHA3GOnEcwNyJueEuJRPVKXdSGu6UOICnuIrYxLjufS84fhoBU4P2OvwMyL8YA8lDYcNQetZT9uyfpPVd4wa1A%3D%3D@rubiks-db.documents.azure.com:10255/?ssl=true'
     },
     production: {}
 };
